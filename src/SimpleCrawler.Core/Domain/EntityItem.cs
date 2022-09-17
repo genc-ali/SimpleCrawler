4d5df833815fea0b2b396501c58464d49d578dc5
recep genc <recep@fullstackdeveloper.net>
2022-09-17 17:31:29 +0100

    adding core library

--- src/SimpleCrawler.Core/Domain/EntityItem.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleCrawler.Core.Domain
-{
-    public abstract class EntityItem<TDto, TDbObject>
-    {
-        public abstract TDbObject GetDbObject();
-        public abstract TDto GetDtoObject();
-
-    }
-}
--- src/SimpleCrawler.Core/MessageQueue/RabbitMq/IRabbitMqClient.cs
+++
@@ -1,7 +0,0 @@
-namespace SimpleCrawler.Core.MessageQueue.RabbitMq
-{
-    public interface IRabbitMqClient
-    {
-        public void SendMessage(byte[] body, string routingKey, System.DateTime addMinutes);
-    }
-}
