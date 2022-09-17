9db4587ea2ebdd8a7b618e750df3b54b33639684
recep genc <recep@fullstackdeveloper.net>
2022-09-17 17:09:50 +0100

    Removed submodule AppLibraries/src

---
+++ src/SimpleCrawler.Core/Crawler/WebCrawler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler.Core.Crawler
+{
+    public  abstract class WebCrawler
+    {
+        public abstract Task<List<Uri>> GetMentions(int pageSize, string keyword, int pageIndex = 0, string initialHashCode = "");
+    }
+}
